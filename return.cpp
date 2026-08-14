dd49843a616e2e94b82580850681e874bf940f43
Floris <149056628+fl0ris1@users.noreply.github.com>
2026-08-14 17:38:52 +0200

    Introduce fullName function and update main
    Added a fullName function to concatenate first and last names.

--- return.cpp
+++ return.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 
 double calc_area(double width, double height); //if value returned, void should be the data type returned
+std::string fullName(std::string firstName, std::string lastName);
 
 int main(){
     
@@ -9,7 +10,15 @@ int main(){
     
     double area = calc_area(width, height);
     
-    std::cout<<area;
+    std::cout<<area<<'\n';
+    
+    
+    std::string firstName = "Floris";
+    std::string lastName = "W";
+    
+    std::string name = fullName(firstName, lastName);
+    
+    std::cout<<name;
     
     return 0;
 }
@@ -17,4 +26,9 @@ int main(){
 double calc_area(double width, double height){ //if value returned, void should be the data type returned
     double result =  width * height;
     return result;
+}
+
+
+std::string fullName(std::string firstName, std::string lastName){
+    return firstName+" "+lastName;
 }
